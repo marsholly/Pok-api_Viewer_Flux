c06d33d62a5416a1035ab01e3a74fc33bd1d91fb
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-28 10:14:12 -0700

    test

--- src/API.js
+++ src/API.js
@@ -3,17 +3,17 @@ import ServerActions from './actions/ServerActions';
 
 const API = {
   searchOnePokemon(id) {
-    $.get(`http://pokeapi.co/api/v2/pokemon/${id}/`, pokemon => {
+    $.get(`//pokeapi.co/api/v2/pokemon/${id}/`, pokemon => {
       ServerActions.receiveOnePokemon(pokemon);
     })
   },
   searchAll() {
-    $.get('https://pokeapi.co/api/v2/pokemon/', pokemons => {
+    $.get('//pokeapi.co/api/v2/pokemon/', pokemons => {
       ServerActions.receiveAllPokemons(pokemons);
     })
   },
   searchGroup(id) {
-    $.get(`https://pokeapi.co/api/v2/pokemon/?offset=${id}`, pokemons => {
+    $.get(`//pokeapi.co/api/v2/pokemon/?offset=${id}`, pokemons => {
       ServerActions.receiveAllPokemons(pokemons);
     })
   }
