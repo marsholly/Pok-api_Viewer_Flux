3a185ce3d0db97b77968bdcde2aea113024a0f0c
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-27 22:37:40 -0700

    rebuild for deployment.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'bootstrap-loader',
+    './node_modules/bootstrap-loader',
     './src/css/style.css',
     './src/index.js',
   ],
-  output: { path: './build', publicPath: '/build/', filename: 'bundle.js' },
+  output: {
+    path: __dirname + '/build',
+    publicPath: '/build/',
+    filename: 'bundle.js'
+  },
   devtool: 'inline-source-map',
   module: {
     loaders: [
@@ -18,12 +22,24 @@ module.exports = {
           presets: ['es2015', 'react']
         }
       },
-      { test: /(\.css)$/, loaders: ['style', 'css'] },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' },
+      { test: /(\.css)$/,
+        loaders: ['style', 'css']
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+      { test: /\.(woff|woff2)$/,
+        loader: 'url?prefix=font/&limit=5000'
+      },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      },
+      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+        loader: 'imports?jQuery=jquery'
+      },
     ]
   }
 }
