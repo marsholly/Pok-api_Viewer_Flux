22ff9e4a76fa60a742eacde4b7e4464f59f9a7e1
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-27 23:04:52 -0700

    changed index.html

--- webpack.config.js
+++ webpack.config.js
@@ -17,29 +17,31 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         query: {
           presets: ['es2015', 'react']
         }
       },
-      { test: /(\.css)$/,
+      {
+        test: /(\.css)$/,
         loaders: ['style', 'css']
       },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'file'
       },
-      { test: /\.(woff|woff2)$/,
+      {
+        test: /\.(woff|woff2)$/,
         loader: 'url?prefix=font/&limit=5000'
       },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url?limit=10000&mimetype=application/octet-stream'
       },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url?limit=10000&mimetype=image/svg+xml'
-      },
-      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
-        loader: 'imports?jQuery=jquery'
-      },
+      }
     ]
   }
 }
@@ -67,22 +69,28 @@ module.exports = {
 //           presets: ['es2015', 'react']
 //         }
 //       },
-//       { test: /(\.css)$/,
+//       {
+// test: /(\.css)$/,
 //         loaders: ['style', 'css']
 //       },
-//       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+//       {
+// test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
 //         loader: 'file'
 //       },
-//       { test: /\.(woff|woff2)$/,
+//       {
+// test: /\.(woff|woff2)$/,
 //         loader: 'url?prefix=font/&limit=5000'
 //       },
-//       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+//       {
+// test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
 //         loader: 'url?limit=10000&mimetype=application/octet-stream'
 //       },
-//       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+//       {
+// test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
 //         loader: 'url?limit=10000&mimetype=image/svg+xml'
 //       },
-//       { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+//       {
+// test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
 //         loader: 'imports?jQuery=jquery'
 //       },
 //     ]
